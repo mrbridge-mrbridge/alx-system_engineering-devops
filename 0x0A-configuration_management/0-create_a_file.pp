8e85111baecc2bd7ed82d322f93340035c0b5c90
root <nanamcroj@gmai.com>
2022-01-03 18:29:47 -0800

    puppet

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,10 @@
 # create a file in /tmp with requirements as given
 
 file { 'school':
+  ensure  => 'present',
   content => 'I love Puppet',
-  permission => '0744',
-  owner => 'www_data',
-  group => 'data',
-  path => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www_data',
+  group   => 'www-data',
+  path    => '/tmp/school',
 }
