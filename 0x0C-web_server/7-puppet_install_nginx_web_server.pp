c2519d1fd29741594118be9591f539e757d14032
root <nanamcroj@gmail.com>
2022-01-09 17:26:42 -0800

    puppet does the most

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,14 @@
+package { 'nginx':
+  ensure  => '1.18.0-0ubuntu1.2',
+}
+file { '/var/www/html/index.nginx-debian.html':
+  ensure  => 'file',
+  content => 'Hello World!',
+  type	  => 'file',
+}
+
+file_line { 'redirect':
+  ensure  => 'present'
+  path	  => '/etc/nginx/sites-available/default'
+  line	  => 'location /redirect_me {\n\treturn 301 https://github.com/mrbridge-mrbridge;\n}',
+}
