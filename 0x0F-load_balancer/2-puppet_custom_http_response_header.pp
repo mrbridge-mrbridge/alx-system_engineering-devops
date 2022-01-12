0abc8192c6c8229aa50f99938404c616ea1d15ad
root <nanamcroj@gmai.com>
2022-01-11 18:59:26 -0800

    p

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -13,14 +13,7 @@ file_line { 'redirect':
   ensure => 'present',
   path   => '/etc/nginx/sites-available/default',
   after  => 'listen 80 default_server;',
-  line   => 'rewrite ^/redirect_me https://github.com/mrbridge-mrbridge permanent;',
-}
-
-file_line { 'add_header':
-  ensure => 'present',
-  path   => '/etc/nginx/sites-available/default',
-  after  => 'listen [::]:80 default_server;',
-  line   => 'add_header	X-Served-By $HOSTNAME;',
+  line   => 'rewrite ^/redirect_me https://github.com/mrbridge-mrbridge permanent;\n\tadd_header X-Served-By $HOSTNAME;',
 }
 
 file { '/var/www/html/index.html':
