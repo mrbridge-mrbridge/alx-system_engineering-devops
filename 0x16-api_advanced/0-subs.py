299fc86f4c313237cdf6510e98f9907fd5874631
root <nanamcroj@gmail.com>
2022-02-15 18:00:51 +0000

    docs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
-"""api that returns number of total subscribers for a subredit"""
-
+"""api that returns number of total subscribers for a subreddit"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """finds number of total subscribers for
-    subreddit"""
-
+    """finds number of total subscribers for subreddit"""
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     headers = {'User-Agent':'mrbridge/v1.0'}
     res = requests.get(url, headers=headers, allow_redirects=False)
