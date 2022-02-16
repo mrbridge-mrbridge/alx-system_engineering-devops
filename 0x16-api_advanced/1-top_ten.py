40e3935943c853b5387a110ce139dd0ae854b98b
root <nanamcroj@gmail.com>
2022-02-16 11:17:59 +0000

    pep

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -3,13 +3,14 @@
 import requests
 
 
-def number_of_subscribers(subreddit):
+def top_ten(subreddit):
     """finds number of total subscribers for subreddit"""
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {'User-Agent': 'mrbridge/v1.0'}
+    headers = {'User-Agent':'mrbridge/v1.0'}
     res = requests.get(url, headers=headers, allow_redirects=False)
     if res.status_code == 404:
-        return 0
+        print('None')
+        return
     else:
         res_data = res.json().get('data').get('subscribers')
     return res_data
