b3dd47a0600d487d8139672109acd704cc91f3ac
root <nanamcroj@gmail.com>
2022-02-16 11:28:45 +0000

    top 10 children

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
-"""api that returns number of total subscribers for a subreddit"""
+"""api that prints title of top ten hot posts for a subreddit"""
 import requests
 
 
 def top_ten(subreddit):
     """finds number of total subscribers for subreddit"""
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {'User-Agent':'mrbridge/v1.0'}
+    url = 'https://www.reddit.com/r/{}/hot/.json?limit=10'.format(subreddit)
+    headers = {'User-Agent': 'mrbridge/v1.0'}
     res = requests.get(url, headers=headers, allow_redirects=False)
     if res.status_code == 404:
         print('None')
         return
     else:
-        res_data = res.json().get('data').get('subscribers')
-    return res_data
+        res_data = res.json().get('data').get('children')
+        for each in res_data:
+            print(each.get('data').get('title'))
