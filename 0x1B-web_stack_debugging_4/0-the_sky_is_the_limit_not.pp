f78729956b51753e65d57671786c4ece16ec5333
root <nanamcroj@gmail.com>
2022-02-24 16:43:56 +0000

    fix ulimit

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,11 @@
 -- fix nginx web server to avoid failed requests
 
+exec { 'set-ulimit':
+  command => 'sed "s/# End of file/* soft nofile 655360\n* hard nofile 655360\n* soft nproc 655360\n* hard nproc 655360\n# End of file/g" /etc/security/limits.conf',
+  path    => '/usr/bin'
+}->
+
 exec { 'restart-nginx':
   command => 'systemctl restart nginx',
-  path    => '/usr/local/bin'
+  path    => '/usr/bin'
 }
