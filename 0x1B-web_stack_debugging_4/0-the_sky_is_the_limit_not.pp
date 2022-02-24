de6d7c4ad221812672a6ba1b26e6182fd802b7c1
root <nanamcroj@gmail.com>
2022-02-24 17:27:30 +0000

    fix

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -2,7 +2,7 @@
 
 # set ulimit
 exec { 'set-ulimit':
-  command => 'sed "s/15/i 5000" /etc/default/nginx',
+  command => 'sed -i "s/15/5000" /etc/default/nginx',
   path    => '/usr/local/bin'
 }->
 
