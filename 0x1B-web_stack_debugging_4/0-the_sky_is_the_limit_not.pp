e78a388724dc54551296f02fc118f09cc68fa4aa
root <nanamcroj@gmail.com>
2022-02-24 18:01:09 +0000

    fix

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -3,7 +3,7 @@
 # Increase the ULIMIT of the default file
 exec { 'fix--for-nginx':
   command => 'sed -i "s/15/4096/" /etc/default/nginx',
-  path    => '/usr/bin/'
+  path    => '/bin/'
 } ->
 
 # Restart Nginx
