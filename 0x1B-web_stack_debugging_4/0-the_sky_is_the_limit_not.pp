a331cb23bfe13d77ef6e1e4d6df68293c59a16b6
root <nanamcroj@gmail.com>
2022-02-24 18:15:25 +0000

    fix

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,8 +1,8 @@
 # fix server to be able to handle higher number of requests
 
 # set nginx ulimit 
-exec { 'fix--for-nginx':
-  command => 'sed "s/^# End/i * soft nofile 655360\n* hard nofile 655360\n* soft nproc 655360\n* hard nproc 655360" /etc/security/limits.conf',
+exec { 'fix-nginx':
+  command => 'sed -i "s/15/5000/" /etc/default/nginx',
   path    => '/bin/'
 } ->
 
